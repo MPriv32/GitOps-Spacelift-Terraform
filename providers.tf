e4a3ddde05cc7d8021da7d6eb0fcd11ce1f5e47c
Mitchell Privett <lahc33@gmail.com>
2022-06-27 18:48:20 -0700

    commented out provider

--- providers.tf
+++ providers.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 provider "aws" {
-  region                   = "us-west-2"
-  shared_credentials_files = ["~/.aws/credentials"]
+  # region                   = "us-west-2"
+  # shared_credentials_files = ["~/.aws/credentials"]
   profile                  = "GitOps"
 }
